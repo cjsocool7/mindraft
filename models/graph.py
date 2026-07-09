cf006b4bb7dac5b49a0212352888afa6c460cfcf
cjsocool7 <riveraian28@gmail.com>
2026-07-09 12:53:58 -0600

    add graph.py

---
+++ models/graph.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+from node import Node
+from edge import Edge
+
+
+@dataclass
+class Graph:
+    nodes: dict = field(default_factory=dict)
+    edges: dict = field(default_factory=dict)
+
+    def add_node(self, node):
+        self.nodes[node.id] = node
+
+    def add_edge(self, edge):
+        self.edges[edge.id] = edge
+
+    def get_all_nodes(self):
+        return self.nodes
+    
+    def get_all_edges(self):
+        return self.edges
